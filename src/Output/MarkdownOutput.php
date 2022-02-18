3cd3b60859ba8ffe7ac10d69db7487844d4fcc4f
DorianWinty <dorianvanlaecke@hotmail.fr>
2022-02-18 23:19:58 +0100

    Handle incomplete reports
    Summary: Format correctly empty or partial reports.
    
    Test Plan: Unit tests from D2537 pass.
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Differential Revision: https://devcentral.nasqueron.org/D2538

--- src/Output/MarkdownOutput.php
+++ src/Output/MarkdownOutput.php
@@ -8,45 +8,60 @@ class MarkdownOutput extends Output {
 
 
         $send = [];
-        $send[] = '# ' . $this->report->title;
-        $send[] = '';
-        foreach ($this->report->sections as $section) {
-            $send[] = '## ' . $section->title;
-            $send[] = '';
-            foreach ($section->entries as $entry) {
+        if ($this->report->sections || $this->report->properties
+            || $this->report->title) {
 
-                $send[] = '### ' . $entry->title;
+            $send[] = '# ' . $this->report->title;
+            $send[] = '';
+
+            foreach ($this->report->sections as $section) {
+                $send[] = '## ' . $section->title;
                 $send[] = '';
-                $send[] = $entry->text;
+                foreach ($section->entries as $entry) {
+
+                    $send[] = '### ' . $entry->title;
+                    $send[] = '';
+                    $send[] = $entry->text;
+                    $send[] = '';
+                }
+            }
+
+            if ($this->report->properties && $this->report->sections) {
+                $send[] = '---';
+                $send[] = '';
+            }
+            if ($this->report->properties) {
+                $properties = $this->report->properties;
+                $propertyMaxLength = 0;
+                $maxValue = 0;
+                foreach ($properties as $key => $value) {
+                    $propertyMaxLength = max($propertyMaxLength, strlen($key));
+                    $maxValue = max($maxValue, strlen($value));
+                }
+                if ($propertyMaxLength < 8) {
+                    $propertyMaxLength = 8;
+                }
+
+                $send[] =
+                    '| Property' . str_repeat(' ', $propertyMaxLength - 8)
+                    . ' | '
+                    . str_repeat(' ', $maxValue) . ' |';
+
+                $send[] = '|' . str_repeat('-', $propertyMaxLength + 2) . '|'
+                          . str_repeat('-', $maxValue + 2) . '|';
+
+                foreach ($properties as $key => $value) {
+                    $send[] =
+                        '| ' . $key . str_repeat(' ',
+                            $propertyMaxLength - strlen($key))
+                        . ' | '
+                        . $value . str_repeat(' ', $maxValue - strlen($value))
+                        . ' |';
+                }
+
                 $send[] = '';
             }
         }
-
-        $send[] = '---';
-        $send[] = '';
-        $properties = $this->report->properties;
-        $propertyMaxLength = 0;
-        $maxValue = 0;
-        foreach ($properties as $key => $value) {
-            $propertyMaxLength = max($propertyMaxLength, strlen($key));
-            $maxValue = max($maxValue, strlen($value));
-        }
-        $propertyMaxLength = max(8, $propertyMaxLength);
-
-        $send[] = '| Property' . str_repeat(' ', $propertyMaxLength - 8) . ' | '
-                  . str_repeat(' ', $maxValue) . ' |';
-
-        $send[] = '|' . str_repeat('-', $propertyMaxLength + 2) . '|'
-                  . str_repeat('-', $maxValue + 2) . '|';
-
-        foreach ($properties as $key => $value) {
-            $send[] =
-                '| ' . $key . str_repeat(' ', $propertyMaxLength - strlen($key))
-                . ' | '
-                . $value . str_repeat(' ', $maxValue - strlen($value)) . ' |';
-        }
-
-        $send[] = '';
 
         return implode("\n", $send);
     }
